931a2b8f3da9c2ea000f8d6ee8f2e974cf63d82c
danylo <danylolitvinchuk@gmail.com>
2022-02-12 23:20:41 +0200

    Initial Commit

---
+++ RedditAggregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditAggregator
+//
+//  Created by Danylo Litvinchuk on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
